28e9c47d9b9218961fd15d32bb63ec25f49c4eda
Rubicones <dmitriypopov1543@gmail.com>
2025-10-29 00:53:20 +0100

    minor fixes

--- src/components/HighlightCard.tsx
+++ src/components/HighlightCard.tsx
@@ -12,7 +12,7 @@ interface Project {
 export default function HighlightCard({ project }: { project: Project }) {
     return (
         <>
-            <div className='hidden md:flex  w-[700px] h-[550px] bg-black rounded-2xl justify-start overflow-hidden relative hover:outline-6 hover:outline-[#C8B936] transition-all duration-200'>
+            <a href={`/project/${project.documentId}`} className='hidden md:flex w-[700px] h-[550px] bg-black rounded-2xl justify-start overflow-hidden relative hover:outline-6 hover:outline-[#C8B936] transition-all duration-200'>
                 <div className='relative w-2/3 h-full'>
                     <Image
                         src={project.Cover.url}
@@ -29,7 +29,9 @@ export default function HighlightCard({ project }: { project: Project }) {
                 <div className='w-1/3 flex flex-col gap-4 font-light p-6 text-xl bg-black'>
                     <div className='flex flex-col'>
                         <span className='text-neutral-500'>Category: </span>
-                        <span className='text-white'>{project.Category.slice(2)}</span>
+                        <span className='text-white'>
+                            {project.Category.slice(2)}
+                        </span>
                     </div>
                     <div className='flex flex-col'>
                         <span className='text-neutral-500'>Date: </span>
@@ -40,8 +42,8 @@ export default function HighlightCard({ project }: { project: Project }) {
                         <span className='text-white'>Personal</span>
                     </div>
                 </div>
-            </div>
-            <div className='flex relative md:hidden w-[300px] aspect-[calc(9.5/16)] flex-col bg-black rounded-2xl justify-start overflow-hidden'>
+            </a>
+            <a href={`/project/${project.documentId}`} className='flex relative md:hidden w-[300px] aspect-[calc(9.5/16)] flex-col bg-black rounded-2xl justify-start overflow-hidden'>
                 <div className='absolute top-0 left-0 h-full w-full z-10'>
                     <Image
                         src={project.Cover.url}
@@ -58,7 +60,9 @@ export default function HighlightCard({ project }: { project: Project }) {
                 <div className='absolute -bottom-[2px] rounded-xl w-[calc(100%+2px)] h-1/3 flex flex-col gap-4 font-light p-6 text-md bg-[#141414] z-20 flex-wrap'>
                     <div className='flex flex-col'>
                         <span className='text-neutral-500'>Category: </span>
-                        <span className='text-white'>{project.Category.slice(2)}</span>
+                        <span className='text-white'>
+                            {project.Category.slice(2)}
+                        </span>
                     </div>
                     <div className='flex flex-col'>
                         <span className='text-neutral-500'>Date: </span>
@@ -69,7 +73,7 @@ export default function HighlightCard({ project }: { project: Project }) {
                         <span className='text-white'>Personal</span>
                     </div>
                 </div>
-            </div>
+            </a>
         </>
     );
 }
