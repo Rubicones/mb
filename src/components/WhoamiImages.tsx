70c0b3255148e24165224caefaed0a8ecf0d7768
Rubicones <dmitriypopov1543@gmail.com>
2025-11-14 18:43:42 +0100

    images replace

--- src/components/WhoamiImages.tsx
+++ src/components/WhoamiImages.tsx
@@ -8,7 +8,7 @@ export default function WhoamiImages() {
     return (
         <div className='w-full md:w-1/3 min-h-[250px] md:min-h-[400px] flex flex-col items-center justify-center relative mr-10 '>
             <Image
-                src='/whoami3.png'
+                src='/whoami2.png'
                 width={200}
                 height={250}
                 className={`absolute object-cover rounded-2xl top-[5px] md:top-[10px] left-[calc(50%-75px+5px)] md:left-[calc(50%-100px+10px)] rotate-12 transition-all duration-300 cursor-pointer hover:scale-125 hover:z-50 ${
@@ -18,7 +18,7 @@ export default function WhoamiImages() {
                 onClick={() => setActiveImage(activeImage === 1 ? null : 1)}
             />
             <Image
-                src='/whoami2.png'
+                src='/whoami3.png'
                 width={200}
                 height={250}
                 className={`absolute object-cover rounded-2xl top-[40px] md:top-[70px] left-[calc(50%-75px-40px)] md:left-[calc(50%-100px-70px)] -rotate-10 transition-all duration-300 cursor-pointer hover:scale-125 hover:z-50 ${
